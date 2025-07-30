6e65dd60af5c8b75d525e7a48a54a3f3fdcbd0cf
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-07-29 19:36:03 -0600

    Menú principal

---
+++ main.py
@@ -0,0 +1,20 @@
+productos = []
+while True:
+    print("\n\n------Lista de acciones-----\n1. Ingresar un producto\n2. Mostrar todos los productos\n3. Buscar un producto\n4. Calcular el inventario\n5. Mostrar los productos por categoría\n6. Salir")
+    select = input("Seleccione una opción: ")
+    match select:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            pass
+        case 6:
+            print("Saliendo del programa...")
+            break
+        case _:
+            print("Por favor elija una opción del menú")
